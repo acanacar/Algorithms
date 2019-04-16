a47e423c4ea9e23c3acbb69614db244c30b54e9b
acanacar <ahmetcanacar.1@gmail.com>
2019-04-16 13:52:09 +0300

    unnecessary study comments are removed and condition is added for better result

--- binaryCountMaxSecondWay.js
+++ binaryCountMaxSecondWay.js
@@ -1,24 +1,19 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
-function dec_to_bin(n){
-  return parseInt(n,10).toString(2);
+function dec_to_bin(n) {
+  return parseInt(n, 10).toString(2)
 }
-
 
 function solution(N) {
   const arr_Binary = dec_to_bin(N).split('')
 
   const indices = []
-  for (const [index,element] of arr_Binary.entries())
-    if (element==='1'){indices.push(index)}
+  for (const [index, element] of arr_Binary.entries())
+    if (element === '1') {indices.push(index)}
 
   const l = []
-  let i;
-  for (i=0;i<indices.length-1;i++){
-    l.push(indices[i+1]-indices[i]-1)
+  let i
+  for (i = 0; i < indices.length - 1; i++) {
+    l.push(indices[i + 1] - indices[i] - 1)
   }
-  const maxx = Math.max.apply(Math,l)
-  return indices,l,maxx
-  // write your code in JavaScript (Node.js 8.9.4)
+  const result = (l.length > 0) ? Math.max.apply(Math, l) : 0
+  return result
 }

eeeaba54aa78f0482d3d98a0bf2bac569d8db09f
acanacar <ahmetcanacar.1@gmail.com>
2019-06-02 22:24:07 +0300

    PermCheck Alternative solution is added

--- PermCheck.py
+++ PermCheck.py
@@ -6,3 +6,15 @@ def solution(A):
             return 0
     else:
         return 0
+
+
+def AlternativeSolution(A):
+    l = len(A)
+    checkarray = [False] * l
+
+    for i in A:
+        if 0 <= i > l:
+            return 0
+        if checkarray[i - 1] == True:
+            return 0
+        checkarray[i - 1] = True

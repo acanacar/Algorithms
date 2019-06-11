6c0fe9fdc4c7dcfe9ba50539f6b8a343eb1f918c
acanacar <ahmetcanacar.1@gmail.com>
2019-06-11 16:59:27 +0300

    GenomicRangeQuery solution is added

---
+++ GenomicRangeQuery.py
@@ -0,0 +1,10 @@
+def solution(S, P, Q):
+    l = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+    result = []
+    for i in range(len(P)):
+        start_ind, end_ind = P[i], Q[i]
+        val = min(list(map(lambda x: l[x], S[start_ind:end_ind+1])))
+        result.append(val)
+    return result
+
+

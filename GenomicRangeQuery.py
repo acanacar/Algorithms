614c360d09d8fbedfb709821a9e4ba8147a692e8
acanacar <ahmetcanacar.1@gmail.com>
2019-06-19 18:13:34 +0300

    some syntaxes are modified

--- GenomicRangeQuery.py
+++ GenomicRangeQuery.py
@@ -1,8 +1,8 @@
 def solution(S, P, Q):
-    l = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+    l_up = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
     result = []
     for i in range(len(P)):
         start_ind, end_ind = P[i], Q[i]
-        val = min(list(map(lambda x: l[x], S[start_ind:end_ind + 1])))
+        val = min(list(map(lambda x: l_up[x], S[start_ind:end_ind + 1])))
         result.append(val)
     return result

0581a44b6d4c433e542de895f28dadaf79426467
acanacar <ahmetcanacar.1@gmail.com>
2019-09-02 12:06:05 +0300

    Triangle codility solution is modified and MaxProductOfThree codility solution is added

--- MaxProductOfThree.py
+++ MaxProductOfThree.py
@@ -1,9 +1,7 @@
 def solution(A):
-    if len(A)<3 or len(A)>100000:
+    if len(A) < 3 or len(A) > 100000:
         raise Exception("invalid input")
     A.sort()
-    if A[0]>1000 or A[-1]<-1000:
-        raise Exception("invalid item")
 
-    if A[0]>0:
-        return A[-1]*A[-2]*A[-3]
+    return max(A[-1]*A[0] * A[1] , A[-1] * A[-2] * A[-3])
+
